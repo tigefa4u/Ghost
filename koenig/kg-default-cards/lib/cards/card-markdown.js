tigefa4u/Ghost

commit b0c495b788e5ce4d4af2d7672027b0af1ccc2868

Koenig - Rename server-side cards
refs https://github.com/TryGhost/Ghost/issues/9311
- match card names to the new generic Koenig card names introduced in https://github.com/TryGhost/Ghost-Admin/commit/95a068615db20664dab578da2e71499ce8b9c50d

koenig/kg-default-cards/lib/cards/card-markdown.js
@@ -1,0 +1,4 @@
+// this card is just an alias of the `markdown` card which is necessary because
+// our markdown-only editor was using the `card-markdown` card name
+let markdownCard = require('./markdown');
+module.exports = markdownCard;

koenig/kg-default-cards/lib/cards/hr.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'card-hr',
+    name: 'hr',
     type: 'dom',
     render(opts) {
         return opts.env.dom.createElement('hr');

koenig/kg-default-cards/lib/cards/html.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    name: 'card-html',
+    name: 'html',
     type: 'dom',
     render(opts) {
         let html = `<div class="kg-card-html">${opts.payload.html}</div>`;

koenig/kg-default-cards/lib/cards/image.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'card-image',
+    name: 'image',
     type: 'dom',
     render(opts) {
         var img = opts.env.dom.createElement('img');

koenig/kg-default-cards/lib/cards/markdown.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'card-markdown',
+    name: 'markdown',
     type: 'dom',
     render: function (opts) {
         var converters = require('../converters'),

commit 13b0264648ab133bc910bd7d4d559ea35aee03bc

Fixed missing export of `card-markdown` card
no issue
- fixes the bug introduced in https://github.com/TryGhost/Ghost/commit/b0c495b788e5ce4d4af2d7672027b0af1ccc2868

koenig/kg-default-cards/lib/cards/card-markdown.js
@@ -1,4 +1,7 @@
+'use strict';
+
 // this card is just an alias of the `markdown` card which is necessary because
 // our markdown-only editor was using the `card-markdown` card name
-let markdownCard = require('./markdown');
-module.exports = markdownCard;
+const markdownCard = require('./markdown');
+
+module.exports = Object.assign(markdownCard, {name: 'card-markdown'});

koenig/kg-default-cards/lib/cards/index.js
@@ -1,6 +1,7 @@
 var hr = require('./hr'),
     html = require('./html'),
     image = require('./image'),
-    markdown = require('./markdown');
+    markdown = require('./markdown'),
+    cardMarkdown = require('./card-markdown');
 
-module.exports = [hr, html, image, markdown];
+module.exports = [hr, html, image, markdown, cardMarkdown];